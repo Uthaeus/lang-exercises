01e06190e961fc809d74008febc316f25d66d7f1
Uthaeus <romanlavery@gmail.com>
2018-03-10 20:12:33 -0700

    hello world python

---
+++ python-exercises/hello_world/template.py
@@ -0,0 +1,30 @@
+
+
+import unittest
+
+def fun(x):
+    return x + 1
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(fun(3), 4)
+
+
+
+
+import unittest
+from unnecessary_math import multiply
+ 
+class TestUM(unittest.TestCase):
+ 
+    def setUp(self):
+        pass
+ 
+    def test_numbers_3_4(self):
+        self.assertEqual( multiply(3,4), 12)
+ 
+    def test_strings_a_3(self):
+        self.assertEqual( multiply('a',3), 'aaa')
+ 
+if __name__ == '__main__':
+    unittest.main()
