d34a1ce721195daa97194786f5c11aee7cdba3cc
Uthaeus <romanlavery@gmail.com>
2018-03-20 16:04:38 -0600

    altered calculator python

---
+++ python-exercises/calculator/calculator_unittest.py
@@ -0,0 +1,14 @@
+import unittest
+from calculator import calculator
+
+class TestCalculator(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+  
+
+
+
+if __name__ == '__main__':
+  unittest.main()
